bb9f7c3a9b7681a55527e6d61ff62cd68cfb23b5
Bryan <bryan@MacBook-Pro-cua-Bryan.local>
2026-07-11 21:22:44 +0800

    fix audio

--- js/features/music.js
+++ js/features/music.js
@@ -5,10 +5,6 @@ const audio = document.getElementById("bgMusic");
 const musicButton = document.getElementById("musicBtn");
 let playing = false;
 
-if (audio) {
-    audio.src = wedding.music;
-}
-
 function setMusicPlaying(isPlaying) {
     playing = isPlaying;
     musicButton?.classList.toggle("playing", isPlaying);
@@ -35,6 +31,11 @@ function pauseMusic() {
 }
 
 export function initMusic() {
+    if (audio && wedding.music) {
+        audio.src = wedding.music;
+        audio.load();
+    }
+
     bindClick(musicButton, () => {
         playing ? pauseMusic() : playMusic();
     });
